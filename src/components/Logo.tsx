79cd03bd60a897b7bb5bfb4e9f39f268a4749560
Roberto Cedeno <150482419+robced@users.noreply.github.com>
2023-12-08 00:42:10 -0800

    feat: create logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div className="xl:text-lg text-lg lg:text-xl 2xl:text-2xl text-text font-semibold">
+            enrri
+            <b className="font-bold italic">quisimo</b>
+        </div>
+    );
+};
+
+export default Logo;
