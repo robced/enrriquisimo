7d573b572e729ee92f896ab21bb203c36c765aae
Devyos <e@robced.com>
2023-12-08 01:01:25 -0800

    fix: variable typo

--- src/components/commons/Button.tsx
+++ src/components/commons/Button.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import {iButton} from "@/types/Button";
+import React, {ReactElement} from 'react';
 
 const Button = ({children, icon, size, color}: iButton) => {
-    const isColoredBotton = color == buttonColor.primary || color === buttonColor.secondary;
+    const isColoredButton = color == buttonColor.primary || color === buttonColor.secondary;
 
     return (
-        <button className={`${color} ${isColoredBotton && "hidden xl:flex"}  ${size ? size : "xl:px-7 md:w-[140px] lg:w-[190px] py-3 2xl:h-header xl:w-[120px]"} flex rounded-xl font-medium items-center justify-center md:text-sm lg:text-md 2xl:text-xl 2xl:tracking-wide shadow`}>
+        <button className={`${color} ${isColoredButton && "hidden xl:flex"}  ${size ? size : "xl:px-7 md:w-[140px] lg:w-[190px] py-3 2xl:h-header xl:w-[120px]"} flex rounded-xl font-medium items-center justify-center md:text-sm lg:text-md 2xl:text-xl 2xl:tracking-wide shadow`}>
             {children}
             <span>{icon}</span>
         </button>
     );
 };
+
+export interface iButton {
+    children: React.ReactNode;
+    color: String;
+    size?: String;
+    icon?: ReactElement;
+}
 
 export const buttonColor = {
     primary: "bg-primary text-background",
