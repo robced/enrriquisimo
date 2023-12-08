fd6643aaa53fd29c58020926af06f5112c107218
Roberto Cedeno <150482419+robced@users.noreply.github.com>
2023-12-08 00:42:03 -0800

    feat: add default export

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Header } from "./sections/Header";
+export { default as Button } from "./commons/Button";
+export { default as Logo } from "./Logo";
+export { buttonColor } from "./commons/Button";
+export * from "./commons/Icons"
